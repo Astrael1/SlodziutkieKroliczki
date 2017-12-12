d138f31784b21e46c65c84cb61c49d5fe2242a53
Astrael <szymon8999@wp.pl>
2017-12-12 15:34:33 +0100

    parameters

--- Slodziotkie_kroliczki/Assets/Scripts/GameManager.cs
+++ Slodziotkie_kroliczki/Assets/Scripts/GameManager.cs
@@ -5,7 +5,8 @@ using UnityEngine.UI;
 
 public class GameManager : Singleton<GameManager> 
 {
-	public GameObject przeszkoda;
+	public GameObject[] przeszkody;
+	public bool[] czyPrzeszkoda = new bool[20];
 	public Transform generator;
 	public GameObject background;
 	public Text scoreText;
@@ -24,7 +25,7 @@ public class GameManager : Singleton<GameManager>
 	// Use this for initialization
 	void Start () 
 	{
-		StartCoroutine (NowaPrzeszkoda() );		
+		StartCoroutine (SpawnujPrzeszkody() );
 	}
 	
 	// Update is called once per frame
@@ -35,6 +36,7 @@ public class GameManager : Singleton<GameManager>
 			velocity = 0;
 			acceleration = 0;
 		}
+			
 		velocity += acceleration * Time.deltaTime;
 		distance += velocity * Time.deltaTime + (acceleration * Time.deltaTime * Time.deltaTime) / 2; //funkcja na droge .-.
 		spawnCheck = (int)distance % obstacleIntervals;
@@ -43,22 +45,35 @@ public class GameManager : Singleton<GameManager>
 		
 	}
 
-	private IEnumerator NowaPrzeszkoda()
+	public void ZamowPrzeszkode(int p)
+	{
+		czyPrzeszkoda [p] = true;
+	}
+
+	private IEnumerator SpawnujPrzeszkody()
 	{
 		
 		//float r = (background.GetComponent<SpriteRenderer> ().bounds.size.x / 2);
 		while (true) 
 		{
+			float x = Random.Range(-5, 5);
 			if(spawnCheck == 10) //10 zeby dac graczowi chwile na ogarniecie
 			{
-				float x = Random.Range(-5, 5);
-				Instantiate (przeszkoda, new Vector3(x, generator.transform.position.y, 1), Quaternion.identity,generator);
+				Instantiate (przeszkody[0], new Vector3(x, generator.transform.position.y, 1), Quaternion.identity,generator);
 
 
 				//NowaPrzeszkoda ();
 				Debug.Log ("Nowa przeszkoda");
 			 	// new WaitForFixedUpdate (przeszkodyCzas);
 				yield return new WaitForSeconds(1); //inaczej spawnuje miliord chmurek na raz, ale teraz pojawia sie max 1 na sekunde, niezaleznie od predkosci
+			}
+			for (int i = 0; i <= przeszkody.GetLength (0); i++) 
+			{
+				if (czyPrzeszkoda [i]) 
+				{
+					Instantiate (przeszkody [i], new Vector3 (x, generator.transform.position.y, 1), Quaternion.identity, generator);
+					czyPrzeszkoda [i] = false;
+				}
 			}
 			yield return null;
 		}
